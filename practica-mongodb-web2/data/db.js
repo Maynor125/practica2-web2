a57039b5b2ded9695f073d10612673ef548b9404
Maynor 125 <maynoldemar@gmail.com>
2023-06-16 14:28:55 -0600

    jiji

--- practica-mongodb-web2/data/db.js
+++ practica-mongodb-web2/data/db.js
@@ -1,7 +1,21 @@
-const e = require('express')
-const mongoose=require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/tareas', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("db conectada"))
-    .catch(e => console.log("La conexion a fallado "+  e))   
+const mongoose = require('mongoose')
+const {MONGODB_URI} = process.env;
 
-module.exports = mongoose;
+if(!MONGODB_URI){
+    throw new Error('MONGODB_URI es necesaria')
+}
+
+const conectDB = async () =>{
+    try {
+      const {connection} = await  mongoose.connect(MONGODB_URI)
+    if(connection.readyState === 1)
+    {
+      console.log("Conectado a mongo db warro")
+      return Promise.resolve(true)
+    }
+    } catch (error) {
+      console.log(error);
+      return Promise.reject(false);
+    }
+}
+module.exports = conectDB
